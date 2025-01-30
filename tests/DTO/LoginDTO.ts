8b6b637327a5b22fc9c7db35f72bc7643d91e0a7
user <sergei.zahharov1@gmail.com>
2025-01-30 17:26:53 +0200

    Lesson-11 | fix formatting

--- tests/DTO/LoginDTO.ts
+++ tests/DTO/LoginDTO.ts
@@ -1,17 +1,13 @@
-export class LoginDTO{
-  username: string;
-  password: string;
+export class LoginDTO {
+  username: string
+  password: string
 
-  private constructor(username: string, password: string){
-    this.username = username;
-    this.password = password;
+  private constructor(username: string, password: string) {
+    this.username = username
+    this.password = password
   }
 
-  static createLoginWithCorrectData(): LoginDTO{
-    return new LoginDTO(
-      process.env.USERNAME || "",
-      process.env.PASSWORD || ""
-    )
+  static createLoginWithCorrectData(): LoginDTO {
+    return new LoginDTO(process.env.USERNAME || '', process.env.PASSWORD || '')
   }
-
 }
--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -2,13 +2,11 @@ import { expect, test } from '@playwright/test'
 import { LoginDTO } from './DTO/LoginDTO'
 import { StatusCodes } from 'http-status-codes'
 
-test.describe("Login tests", async()=>{
-
+test.describe('Login tests', async () => {
   test('Successful authorization ', async ({ request }) => {
-const response = await request.post('https://backend.tallinn-learning.ee/login/student',{
-  data: LoginDTO.createLoginWithCorrectData()
-})
+    const response = await request.post('https://backend.tallinn-learning.ee/login/student', {
+      data: LoginDTO.createLoginWithCorrectData(),
+    })
     expect(response.status()).toBe(StatusCodes.OK)
   })
 })
-
